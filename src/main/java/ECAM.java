e7f6862458d3b9f52cace7c92270147b3dae50eb
Benjamin Vandenbussche <benjvdb9@hotmail.com>
2018-10-24 09:36:42 +0200

    Filled src

---
+++ src/main/java/ECAM.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class ECAM {
+    private ECAM() {
+        List<String> teachers;
+    }
+
+    public void getInstance() {
+        System.out.println("Test");
+    }
+}
