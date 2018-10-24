a9ec86b671a0789b043c3c290b32cff32057e494
Benjamin Vandenbussche <benjvdb9@hotmail.com>
2018-10-24 10:13:42 +0200

    Added Human class

---
+++ src/main/java/Human.java
@@ -0,0 +1,13 @@
+public class Human {
+    private final String name;
+    private final String lastname;
+
+    public Human(String name, String lastname){
+        this.name = name;
+        this.lastname = lastname;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
